023f4f7e5407aeafc6346bed53b66c794ccb66d6
huyvu12300 <vu50861@gmail.com>
2024-04-16 23:57:44 +0700

    first commit

---
+++ src/test/java/cinemawebsite/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cinemawebsite.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
