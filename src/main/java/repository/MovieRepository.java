1c5baaf31e81e9a4ead53826143661bcd59037a0
= <nguyentrananhduy.234@gmail.com>
2024-04-17 10:55:01 +0700

    add MovieRepository

--- src/main/java/repository/MovieRepository.java
+++ src/main/java/repository/MovieRepository.java
@@ -1,2 +1,15 @@
-package repository;public interface MovieRepository {
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.markowski.kinoteatr.model.Repertoire;
+
+import java.util.List;
+
+@Repository
+public interface RepertoireRepository extends JpaRepository<Repertoire, Long> {
+
+    List<Repertoire> findByMovieId(final Long movieId);
+
+    List<Repertoire> findBySpectacleId(final Long spectacleId);
 }
